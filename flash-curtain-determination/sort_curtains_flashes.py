298b23fa24454535af402ea958223cbeb0a30010
Mykhaylo Mike Shumko <msshumko@gmail.com>
2017-11-17 13:25:02 -0700

    Wrote the microburst catalogue reader, and started plotting the times to get an idea how easy it will be to identify curtains and flashes.

--- flash-curtain-determination/sort_curtains_flashes.py
+++ flash-curtain-determination/sort_curtains_flashes.py
@@ -1,7 +1,9 @@
 # This program will sort microbursts by curtains and flashes and output the
 # data to the ./../data/<flash or curtain>_catalogues.
 from datetime import datetime, timedelta
+import matplotlib.pyplot as plt
 import numpy as np
+import dateutil.parser 
 import csv
 import os
 
@@ -11,6 +13,10 @@ class SortMicrobursts:
         self._loadMicroburstCatalogues(indir)
         return
 
+    def findFlashes(self):
+
+        return
+
     def _loadMicroburstCatalogues(self, fDir):
         """
         This function will load in the microburst catalogues
@@ -18,13 +24,31 @@ class SortMicrobursts:
         """
         
         for sc_id in ['A', 'B']:
-            fName = 'AC6{}_{}_micobursts.txt'.format(
+            fName = 'AC6{}_{}_microbursts.txt'.format(
                 sc_id, self.date.date())
-            self._dataSkelletons(sc_id, os.path.join(fDir, fName))
+            data = self._inputDataSkelletons(sc_id, os.path.join(fDir, fName))
 
+            with open(os.path.join(fDir, fName)) as f:
+                reader = csv.reader(f)
+                next(reader) # Skip header
+                next(reader)
+
+                for i, line in enumerate(reader): # Read in the data
+                    data[i] = line
+                # Now format data array into a dictionary for each spacecraft
+                dataDict = {}
+                for i, key in enumerate(self.keys):
+                    dataDict[key] = data[:, i]
+                # Convert datetimes
+                dataDict['dateTime'] = np.array([dateutil.parser.parse(i) for i in dataDict['dateTime']])
+                # Assign data to correct attribute
+                if sc_id.upper() == 'A':
+                    self.dataA = dataDict
+                else:
+                    self.dataB = dataDict
         return
 
-    def _dataSkelletons(self, sc_id, fPath):
+    def _inputDataSkelletons(self, sc_id, fPath):
         """
         Create empty dictionary and arrays for microburst catalogues. 
         """
@@ -32,23 +56,27 @@ class SortMicrobursts:
         with open(fPath, 'r') as f:
             N = sum(1 for row in f) - 2
 
-        # Get data keys from file
+        # Get data keys and data from file
         with open(fPath, 'r') as f:
             reader = csv.reader(f)
             next(reader) # Skip first line of header
             self.keys = next(reader)
-            self.keys[0] = self.keys[0][2:] # Remove comment and empty space chars
-
-            
-            data = np.nan*np.ones((N, len(self.keys)))
-
-            for i, line in enumerate(reader): # Read rest of file
-                data[i] = line
-
-            print(data)
-        return
+            # Remove comment and empty space chars
+            self.keys[0] = self.keys[0][2:] 
+            # Empty data file
+            data = np.nan*np.ones((N, len(self.keys)), dtype=object)
+        return data
 
 if __name__ == '__main__':
     inDir = os.path.abspath('./../data/microburst_catalogues/')
     date = datetime(2016, 9, 30)
     sorter = SortMicrobursts(inDir, date)
+
+    # Plot the times to understand how closely they match
+    fig, ax = plt.subplots()
+    for burst in sorter.dataA['dateTime']:
+        ax.axvline(burst, ymax=0.5, c='k')
+    for burst in sorter.dataB['dateTime']:
+        ax.axvline(burst, ymin=0.5, ymax=1, c='b')
+    ax.set_xlim(sorter.dataA['dateTime'][0], sorter.dataA['dateTime'][-1])
+    plt.show()

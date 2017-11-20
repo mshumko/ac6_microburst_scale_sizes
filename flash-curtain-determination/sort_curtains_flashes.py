64643cc2d5c2c1fa22636ffeb26a03943c15820a
Mykhaylo Mike Shumko <msshumko@gmail.com>
2017-11-20 10:36:52 -0700

    Now the data is created correctly, and I am writing a save function to save in a CSV format.

--- flash-curtain-determination/sort_curtains_flashes.py
+++ flash-curtain-determination/sort_curtains_flashes.py
@@ -10,12 +10,13 @@ import csv
 import os
 
 class SortMicrobursts:
-    def __init__(self, indir, date=None):
+    def __init__(self, indir, date=None, flashesSwitch=True):
         self.date = date
         self._loadMicroburstCatalogues(indir)
+        self.flashesSwitch = flashesSwitch
         return
 
-    def simpleFindFlashes(self, thresh=0.2):
+    def simpleFindMatches(self, thresh=0.2):
         """
         Loop over list A and look for microbursts in list B at
         times +/- thresh. If In_Track_Lag < thresh, give 
@@ -24,13 +25,11 @@ class SortMicrobursts:
         self.flashes = np.nan*np.ones((0, len(self.keys)+2))
         dT = timedelta(seconds=thresh)
         
-        print(self.keys)
-        
         for (i, tA) in enumerate(self.dataA['dateTime']):
             matchIdt = np.where(
                 (tA >= self.dataB['dateTime'] - dT) & 
                 (tA <= self.dataB['dateTime'] + dT))
-            if len(matchIdt) == 0: 
+            if len(matchIdt[0]) != 1: 
                 continue # If 0 or > 1 matches found, ignore.
                 
             # Save the same keys as input data except save
@@ -38,19 +37,22 @@ class SortMicrobursts:
             # dos1rates.
             # Calc stats on separation and location
             stats = []
-            print(matchIdt)
             for key in self.keys[2:]:
-                print([self.dataA[key][i], 
-                    self.dataB[key][matchIdt[0]]])
                 stats.append(np.mean([self.dataA[key][i], 
                     self.dataB[key][matchIdt[0][0]] ]))
             dataLine = np.array(
                 [tA, self.dataA['dos1rate'][i], 
-                self.dataB['dateTime'][matchIdt[0]],
-                self.dataB['dos1rate'][matchIdt[0]] ])
-            dataLine = np.concatenate(dataLine, stats)
-            print(dataLine)
-        
+                self.dataB['dateTime'][matchIdt[0][0]],
+                self.dataB['dos1rate'][matchIdt[0][0]] ])
+            dataLine = np.concatenate((dataLine, stats))
+
+            # Now save to a large 2-D array
+            self.flashes = np.row_stack((self.flashes, dataLine))
+        print(self.flashes)
+        return
+
+    def saveData(self, ):
+
         return
 
     def _loadMicroburstCatalogues(self, fDir):
@@ -113,8 +115,8 @@ class SortMicrobursts:
 
 if __name__ == '__main__':
     startTime = time.time()
-    inDir = os.path.abspath('./../data/microburst_catalogues/')
-    #date = datetime(2017, 1, 11)
-    sorter = SortMicrobursts(inDir)
-    sorter.simpleFindFlashes()
+    inDir = os.path.abspath('./../data/daily_microburst_catalogues/')
+    date = datetime(2017, 1, 11)
+    sorter = SortMicrobursts(inDir, date=date)
+    sorter.simpleFindMatches()
     print('Run time: {}'.format(round(time.time()-startTime)))

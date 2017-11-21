ae1ae3865e9b4207cf54cc5f36b6644650e909df
Mykhaylo Mike Shumko <msshumko@gmail.com>
2017-11-20 18:02:42 -0700

    Started working on appending Kp index to the data.

---
+++ flash-curtain-determination/append_goemag_indicies.py
@@ -0,0 +1,17 @@
+# This file appends Kp index to the curtain and flash catalogues.
+import sys
+import os
+import csv
+import numpy as np
+
+class AppendGeoMagIdx:
+    """
+
+    """
+    def __init__(self):
+
+        return
+
+    def appendKp(self):
+
+        return

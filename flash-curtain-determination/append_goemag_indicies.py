72ae94750ab4b0ed066677631a4028813381dd33
Mykhaylo Mike Shumko <msshumko@gmail.com>
2017-11-22 15:59:48 -0700

    Finished writing the append AE index code. Since the AE files are so large, this process takes ~10 minutes. Can be improved, but unecessary for now.

--- flash-curtain-determination/append_goemag_indicies.py
+++ flash-curtain-determination/append_goemag_indicies.py
@@ -22,13 +22,49 @@ class AppendGeoMagIdx:
         self.indexDir = indexDir
         self.dataPath = dataPath
 
-        # Load data and Kp
-        self._loadData()
-        self._loadIndicies()
+        self._loadData() # Load microburst data
+
+        if self.iType.lower() == 'kp':
+            self._loadKp() # Load kp index
+        elif self.iType.lower() == 'ae':
+            self._loadAe()
         return
 
-    def appendKp(self):
-        
+    def appendIndex(self, timeKey='dateTimeA'):
+        """
+        This function will loop over the microburst data set and look for
+        matching geomagnetic indicies.
+        """
+        self.matchingIndex = np.nan*np.ones(len(self.dataDict['lat']), dtype=int)
+
+        # Loop over microburst times
+        for (i, t) in enumerate(self.dataDict[timeKey]):
+            idx = np.where((t >= self.indexTimes[:-1]) & 
+                (t < self.indexTimes[1:]))[0]
+            print(t, idx, self.indexTimes[idx])
+            if len(idx) == 0:
+                break
+            self.matchingIndex[i] = self.index[idx]
+        print(self.matchingIndex)
+        return
+
+    def saveData(self):
+        """
+        This file saves the microburst data to test.txt for now...
+        """
+        with open(self.dataPath, 'w', newline='') as f:
+            writer = csv.writer(f)
+
+            # Header
+            self.keys[0] = '# {}'.format(self.keys[0])
+            writer.writerow(
+                np.concatenate((self.keys, [self.iType.upper()]) 
+                ))
+            # Loop over lines and save file.
+            for (i, line) in enumerate(self.data):
+                writer.writerow(
+                    np.concatenate( (line, [self.matchingIndex[i]]) )
+                    )
         return
 
     def _loadData(self):
@@ -36,7 +72,7 @@ class AppendGeoMagIdx:
         This function will load in the microburst catalogues
         from both spacecraft
         """
-        data = self._inputDataSkelletons(self.dataPath)
+        self.data = self._inputDataSkelletons(self.dataPath)
 
         with open(self.dataPath, 'r') as f:
             reader = csv.reader(f)
@@ -44,11 +80,11 @@ class AppendGeoMagIdx:
             next(reader)
 
             for i, line in enumerate(reader): # Read in the data
-                data[i] = line
+                self.data[i] = line
             # Now format data array into a dictionary for each spacecraft
             self.dataDict = {}
             for i, key in enumerate(self.keys):
-                self.dataDict[key] = data[:, i]
+                self.dataDict[key] = self.data[:, i]
             # Loop over all keys but datetimes and set the array types to be floats.
             for key in itertools.filterfalse(lambda x:'dateTime' in x, self.keys):
                 self.dataDict[key] = self.dataDict[key].astype(float)
@@ -78,7 +114,7 @@ class AppendGeoMagIdx:
             data = np.nan*np.ones((N, len(self.keys)), dtype=object)
         return data
 
-    def _loadIndicies(self):
+    def _loadKp(self):
         """
         This function will load in the Kp data
         """
@@ -87,7 +123,7 @@ class AppendGeoMagIdx:
 
         # Loop over years in the data and load in that index.
         dataYears = sorted(set([i.year for i in self.dataDict['dateTimeA']]))
-        for year in dataYears:
+        for year in [2014]:
             fName = '{}_{}.txt'.format(year, self.iType.lower())
             indexData = spacepy.datamodel.readJSONheadedASCII(
                 os.path.join(self.indexDir, fName))
@@ -97,10 +133,36 @@ class AppendGeoMagIdx:
         self.indexTimes = np.array([dateutil.parser.parse(t) for t in self.indexTimes])
         return
 
+    def _loadAe(self, aeType='AE', headerSize=15):
+        """
+
+        """
+        self.indexTimes = np.array([], dtype=object)
+        self.index = np.array([], dtype=int)
+
+        dTypes = ['AE', 'AU', 'AL', 'A0']
+        idx = 3 + dTypes.index(aeType)
+        
+        dataYears = sorted(set([i.year for i in self.dataDict['dateTimeA']]))
+        for year in dataYears:
+            fName = '{}_{}.txt'.format(year, self.iType.lower())
+            with open(os.path.join(self.indexDir, fName), 'r') as f:
+                reader = csv.reader(f, skipinitialspace=True, delimiter=' ')
+                data = np.array(list(reader))
+
+                # Skip the header and convert times
+                self.indexTimes = np.append(self.indexTimes, 
+                    [dateutil.parser.parse(' '.join(line[0:2])) for line in 
+                    data[headerSize:]])
+                # Save indicies.
+                indicies = list(map(float, [line[idx] for line in data[headerSize:]]))
+                self.index = np.append(self.index, indicies)
+        return
+
 if __name__ == '__main__':
-    iType = 'Kp'
-    dataType = 'flashes'
-    indexDir = '/home/mike/research/firebird/data_processing/geomag_indicies/indicies'
+    iType = 'ae'
+    dataType = 'curtains'
+    indexDir = '/home/mike/research/geomag_indicies/ae'
 
     if dataType == 'flashes':
         dataPath = ('/home/mike/research/ac6-microburst-scale-sizes/data/'
@@ -110,3 +172,5 @@ if __name__ == '__main__':
                     'curtain_catalogues/curtains_catalogue.txt')
 
     appendObj = AppendGeoMagIdx(iType, dataPath, indexDir)
+    appendObj.appendIndex()
+    appendObj.saveData()

33e315df9fcf54850b4372d45a6fe79456c147c0
Mykhaylo Mike Shumko <msshumko@gmail.com>
2017-12-11 09:48:29 -0700

    Started writing the script to visialuze the microburst detections.

---
+++ flash-curtain-determination/plotMatchedBursts.py
@@ -0,0 +1,8 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import sys
+import os
+
+sys.path.append('/home/mike/research/ac6-microburst-scale-sizes/stats/')
+
+class PlotMicroburstMatches		

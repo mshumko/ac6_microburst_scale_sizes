f95a523ee809a6df7f69401d5c704b53d6ba8857
Mykhaylo Mike Shumko <msshumko@gmail.com>
2018-03-14 15:23:27 -0600

    Finished writing the normalization histogram code. I can feed it a dictionary of keys to filter the data by.
    2D histogram normalization is not implemented yet.

--- stats/hist.py
+++ stats/hist.py
@@ -5,6 +5,7 @@ from datetime import datetime, timedelta
 import csv
 import numpy as np
 import sys
+import os
 
 # Import personal libraries
 sys.path.insert(0, '/home/mike/research/mission-tools/ac6')
@@ -12,7 +13,7 @@ import read_ac_data
 
 class Hist1D:
     def __init__(self, d=None, sc_id='B', startDate=datetime(2014, 1, 1),
-                 endDate=datetime.now(), filterDict={}):
+                 endDate=datetime.now(), filterDict={}, flag=True):
         """
         This class calculates the 1D histograms as a function of distance
         for the various filter parameters. 
@@ -28,16 +29,23 @@ class Hist1D:
         self.dates = [startDate + timedelta(days=i) for i in range(dDays)] 
         self.sc_id = sc_id  
         self.filterDict = filterDict
+        self.flag = flag
         return
 
-    def __iter__(self):
+    def loop_data(self):
         """
-        This class will iterate over the dats from startDate to endDate, 
-        load data, filter it, and histgram it.
-        """
-        return self
 
-    def get_data(self, date):
+        """
+        for day in self.dates:
+            self.load_day_data(day)
+            if self.ac6data is None:
+                continue # If data file is empty
+            ind = self.filterData()
+            self.hist_data(ind)
+
+        return
+
+    def load_day_data(self, date):
         """
         This generator function will load in one day of data at a 
         time, and return control to user.
@@ -48,7 +56,8 @@ class Hist1D:
                 self.sc_id, date, dType='10Hz', plot=False)
         except AssertionError as err:
             if ( ('None or > 1 AC6 files found' in str(err)) or
-                ('Error, the data is not 2D (Empty file?)' in str(err)) ):
+                ('File is empty!' in str(err)) ):
+                self.ac6data = None
                 return None
             else:
                 raise
@@ -58,13 +67,39 @@ class Hist1D:
         """
         This function will filter the AC-6 data.
         """
-        ind = len(self.ac6data[self.ac6data.keys()[0]])
+        if self.flag:
+            ind = np.where(self.ac6data['flag'] == 0)[0]
+        else:
+            ind = range(len(self.ac6data['flag']))
+
         for key, value in self.filterDict.items():
-            idx = np.where((self.ac6data[key] > value.min()) & 
-                            (self.ac6data[key] < value.max()))[0]
+            idx = np.where((self.ac6data[key] > np.min(value)) & 
+                            (self.ac6data[key] < np.max(value)))[0]
             ind = np.intersect1d(ind, idx)
         return ind
 
+    def hist_data(self, ind):
+        """
+        This method will histrogram the total distance data.
+        """
+        H, _ = np.histogram(self.ac6data['Dist_Total'][ind], bins=self.d)
+        self.count += H/10
+
+        print(self.count)
+        return
+
+    def save_data(self, fPath):
+        """
+        This method saves the normalization data to a csv file.
+        """
+        with open(fPath, 'w', newline='') as f:
+            w = csv.writer(f)
+            w.writerow(['Separation [km]', 'Seconds'])
+
+            for (d, s) in zip(self.d, self.count):
+                w.writerow([d, s])
+        print('Saved data to {}'.format(os.path.basename(fPath)))
+        return
 
 class Hist2D(Hist1D):
     def __init__(self):
@@ -75,8 +110,16 @@ class Hist2D(Hist1D):
         pass
 
 if __name__ == '__main__':
-    h = Hist1D()
-    #print(f)
-    for i, d in enumerate(h):
-        print(i)
-        print(d)
+    # This script will loop over the intervals in Lbins, and 
+    # make normalization histograms for all of them.
+    Lbins = [3, 4, 5, 6, 7]
+    for (lL, uL) in zip(Lbins[:-1], Lbins[1:]):
+        h = Hist1D(filterDict={'Lm_OPQ':[lL,uL]})
+        h.loop_data()
+        h.save_data('/home/mike/research/ac6-microburst-scale-sizes'
+                    '/data/norm/ac6_norm_{}_L_{}.txt'.format(lL, uL))
+
+    # h = Hist1D()
+    # h.loop_data()
+    # h.save_data('/home/mike/research/ac6-microburst-scale-sizes'
+    #             '/data/norm/ac6_norm_all.txt')

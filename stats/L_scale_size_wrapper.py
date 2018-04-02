92016ee001f2b63f5f6f39d76fced8e19fdc5d58
mshumko <msshumko@gmail.com>
2018-04-01 21:53:33 -0600

    Rennamed Files

--- stats/L_scale_size_wrapper.py
+++ stats/L_scale_size_wrapper.py
@@ -1,7 +1,7 @@
 # this script explores the scale size dependence on L shell
 import matplotlib.pyplot as plt
 from datetime import datetime
-import scale_size
+import leo_scale_sizes
 import os
 
 # Set L shell parameters
@@ -13,9 +13,9 @@ for (lL, uL) in zip(Lrange[:-1], Lrange[1:]):
     cName = 'flash_catalogue_v2_sorted.txt'
     # Normalization dir and name
     nDir = '/home/mike/research/ac6-microburst-scale-sizes/data/norm'
-    nName = 'ac6_norm_{}_L_{}.txt'.format(lL, uL)
+    nName = 'ac6_norm_{}_L_{}.csv'.format(lL, uL)
     
-    ss = scale_size.ScaleSizeDist(os.path.join(cDir, cName), os.path.join(nDir, nName), 
+    ss = leo_scale_sizes.ScaleSizeDist(os.path.join(cDir, cName), os.path.join(nDir, nName), 
                     fltDict={'burstType':'flash', 'Lm_OPQ':[lL,uL]})
 
     # Plot data
@@ -27,5 +27,4 @@ for (lL, uL) in zip(Lrange[:-1], Lrange[1:]):
     ax[1].set_title('Normalization')
 
     plt.savefig(('/home/mike/Dropbox/0_grad_work/ac6-flashes-curtains/'
-                'plots/{}_scale_sizes/'
-                'flash_scale_size_{}_L_{}.png').format(datetime.now().date(), lL, uL))
+                'plots/{}/microburst_scale_size_{}_L_{}.png').format(datetime.now().date(), lL, uL))

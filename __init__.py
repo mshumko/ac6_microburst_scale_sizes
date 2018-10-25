d2c2c1d865ce9caad6e1cb147d405e55817172fb
mike <msshumko@gmail.com>
2018-10-25 09:31:07 -0600

    Renamed folders.

---
+++ __init__.py
@@ -0,0 +1,8 @@
+import stats
+import validation
+import rabbit_holes
+import microburst_detection
+import flash_curtain_determination
+
+print('Import __init__ from root directory '
+      'of ac6_microburst_scale_sizes repo.')

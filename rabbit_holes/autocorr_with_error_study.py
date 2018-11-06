f89edf4f014212cd6f382659faf985da75c0f766
mike <msshumko@gmail.com>
2018-11-05 18:53:27 -0700

    Divided the counts by 10 to get counts/bin.

--- rabbit_holes/autocorr_with_error_study.py
+++ rabbit_holes/autocorr_with_error_study.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 
 N = 1000
 y = np.array([250.001, 270.001, 270.001, 320.002, 370.002, 330.002, 329.969,
-       310.002, 270.001])
+       310.002, 270.001])/10
 cc = np.zeros(N)
 
 for i in range(N):
@@ -13,7 +13,7 @@ for i in range(N):
     cc[i] = cc_unnormalized/np.sqrt(len(y)*len(y)*np.var(y_noise)*np.var(y))
 
 fig, ax = plt.subplots()
-ax.hist(cc, range=[.75, 1])
+ax.hist(cc, range=[0, 1])
 ax.text(0.01, 0.99, 'N={}'.format(N), transform=ax.transAxes, va='top')
 ax.set_ylabel('Count Rate'); ax.set_xlabel('Cross-correlation')
 plt.show()

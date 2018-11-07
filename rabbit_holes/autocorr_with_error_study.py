fb6e24861f8ba1f92da196a45696c07b683f696e
mike <msshumko@gmail.com>
2018-11-06 19:03:09 -0700

    Added an option to include the full error.

--- rabbit_holes/autocorr_with_error_study.py
+++ rabbit_holes/autocorr_with_error_study.py
@@ -7,8 +7,17 @@ y = np.array([250.001, 270.001, 270.001, 320.002, 370.002, 330.002, 329.969,
        310.002, 270.001])/10
 cc = np.zeros(N)
 
+error = ''
+
 for i in range(N):
     y_noise = np.random.poisson(y)
+
+    # If error mode is 'full', then assume an error from the count rates and 
+    # mean subtraction. Add these uncertanties in quadreture.
+    if error == 'full':
+        residuals = y - y_noise
+        y_noise = y + np.sign(residuals)*np.sqrt(residuals**2 + np.var(y))
+        
     cc_unnormalized = np.correlate(y-y.mean(), y_noise-y_noise.mean(), mode='valid')
     cc[i] = cc_unnormalized/np.sqrt(len(y)*len(y)*np.var(y_noise)*np.var(y))
 

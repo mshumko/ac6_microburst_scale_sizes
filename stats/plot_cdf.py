d3ff0e9f163d13e93efad6c91f5c3a1a0cc7c06b
mike <msshumko@gmail.com>
2019-01-07 10:03:14 -0700

    Playing around with the script parameters. A few observations.
    1. At separations > 50 km, there are lots of coincident events.
    They appear to be due to cross-correlared noise.
    2. One way I can take care of this is to check that the detection
    is N x above e.g. the 10% baseline.
    3. Many of the noise detections come from April 2015.
    I need to ask why this month (especially after the 20th) are significaly more noisy.
    AE was high on some days.

--- stats/plot_cdf.py
+++ stats/plot_cdf.py
@@ -2,34 +2,44 @@ import numpy as np
 import matplotlib.pyplot as plt
 import dateutil.parser
 
-# converters = {0:dateutil.parser.parse, 
-#             -1:dateutil.parser.parse, 
-#             -2:dateutil.parser.parse}
+converters = {0:dateutil.parser.parse, 
+            -1:dateutil.parser.parse, 
+            -2:dateutil.parser.parse}
 
-bins = np.arange(0, 50, 5)
+bins = np.arange(0, 200, 5)
 frac = np.nan*np.zeros(len(bins)-1)
-CC_thresh = 0.7
+CC_thresh = 0.8
 
 dtypes = (object, float, float, float, float, float, float, float, 
         float, float, float, float, float, float, float, object, object)
 data = np.genfromtxt('coincident_microburst_test.csv', delimiter=',',
-                    names=True, dtype=dtypes)
+        names=True, dtype=dtypes)#, converters=converters)
+# years = np.array([t.year for t in data['dateTime']])
+# months = np.array([t.month for t in data['dateTime']])
 
 for i, (lower_edge, upper_edge) in enumerate(zip(bins[:-1], bins[1:])):
-    # Find microbursts in bin
-    idsep = np.where((data['Dist_Total'] > lower_edge) & 
-            (data['Dist_Total'] <= upper_edge))[0]
-    idCoincident = np.where((data['time_cc'][idsep] >= CC_thresh) & 
-                    (data['time_cc'][idsep] > data['space_cc'][idsep]))[0]
-    frac[i] = len(idCoincident)/len(idsep)
+        # Find microbursts in bin
+        idsep = np.where(
+                        (data['Dist_Total'] > lower_edge) & 
+                        (data['Dist_Total'] <= upper_edge) &
+                        #np.logical_not((years == 2015) & (months == 4)) &
+                        (np.abs(data['Lm_OPQ']) < 8) & (data['dos1rate'] > 100)
+                        )[0]
+        idCoincident = np.where((data['time_cc'][idsep] >= CC_thresh) & 
+                        (data['time_cc'][idsep] > data['space_cc'][idsep]))[0]
+        if len(idsep):
+                frac[i] = len(idCoincident)/len(idsep)
+
+        if lower_edge >= 85 and lower_edge <= 90:
+                print(lower_edge, upper_edge, data['dateTime'][idsep], '\n')
 
 pdf = np.convolve([-1, 1], frac, mode='valid')
 
-fig, ax = plt.subplots(2, sharex=True)
-ax[0].bar(bins[:-1], frac, width=(bins[1]-bins[0])*0.8)
-ax[0].set_ylabel('CDF')
+fig, ax = plt.subplots(1, sharex=True)
+ax.bar(bins[:-1], frac, width=(bins[1]-bins[0])*0.8)
+ax.set_ylabel('CDF')
 
-ax[1].bar(bins[1:-1], pdf, width=(bins[1]-bins[0])*0.8)
-ax[1].set_ylabel('PDF')
+# ax[1].bar(bins[1:-1], pdf, width=(bins[1]-bins[0])*0.8)
+# ax[1].set_ylabel('PDF')
 
 plt.show()

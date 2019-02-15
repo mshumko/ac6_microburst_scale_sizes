494002c856e30eaa11c43bdd8853221ec88f968e
Mykhaylo Mike Shumko <msshumko@gmail.com>
2019-02-15 14:41:45 -0700

    Finished writing the script for the most common_cdf_bins to find the five most common L-MLT-AE-separation bins that make up the microburst CDF.

--- stats/common_cdf_bins.py
+++ stats/common_cdf_bins.py
@@ -2,6 +2,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import dateutil.parser
+import itertools
 
 import pandas as pd
 
@@ -41,4 +42,52 @@ data = data[
 data = data[data['peak_std'] > 2]
 # Filter out ambigious CCs with curtains
 data = data[data['time_cc'] > data['space_cc']+curtain_thresh]
-#bins = np.arange(2, 100, 3)
+
+### BIN THE DETECTIONS ###
+# Now bin the data by L-MLT-AE and separation.
+L_bins=np.arange(4, 8, 1) 
+MLT_bins=np.arange(0, 15, 1)
+AE_bins=np.arange(0, 600, 100)
+sep_bins = np.arange(0, 100, 5)
+
+def index_to_name(LL, MLTMLT, AEAE, i, j, k):
+    s = '{}_L_{}_{}_MLT_{}_{}_AE_{}'.format(
+        LL[i, j, k], LL[i, j+1, k], MLTMLT[i, j, k], MLTMLT[i+1, j, k],
+        AEAE[i, j, k], AEAE[i, j, k+1])
+    return s
+
+LL, MLTMLT, AEAE = np.meshgrid(L_bins, MLT_bins, AE_bins)
+N = np.nan*np.zeros_like(LL)
+lL, lMLT, lAE = LL.shape
+
+N_most_common = 5
+# most_common_bins = np.array([[None]*(len(sep_bins)-1)]*N_most_common)
+most_common_bins = np.ones((len(sep_bins)-1, 2+N_most_common*2), dtype=object)
+
+# Loop over the L, MLT, AE bins
+for s_i, (si, sf) in enumerate(zip(sep_bins[:-1], sep_bins[1:])):
+    N = np.nan*np.zeros(((lL-1)*(lMLT-1)*(lAE-1), 4))
+    n = 0
+
+    for i, j, k in itertools.product(range(lL-1), range(lMLT-1), range(lAE-1)):
+        data_copy = data[
+            (data['Lm_OPQ'] > LL[i, j, k]) & (data['Lm_OPQ'] < LL[i, j+1, k]) &
+            (data['MLT_OPQ'] > MLTMLT[i, j, k]) & (data['MLT_OPQ'] < MLTMLT[i+1, j, k]) &
+            (data['AE'] > AEAE[i, j, k]) & (data['AE'] < AEAE[i, j, k+1]) & 
+            (data['Dist_Total'] > si) & (data['Dist_Total'] < sf)
+            ]
+        N[n] = [i, j, k, data_copy.shape[0]]
+        n += 1
+    df = pd.DataFrame(data=N, dtype=int)
+    #print(df.nlargest(N_most_common, 3))
+    nlargest = df.nlargest(N_most_common, 3)
+    for row in range(nlargest.shape[0]):
+        ii, jj, kk, nn = nlargest.iloc[row]
+        most_common_bins[s_i, 0:2] = [si, sf]
+        most_common_bins[s_i, 2+row] = index_to_name(LL, MLTMLT, AEAE, ii, jj, kk)
+        most_common_bins[s_i, 2+N_most_common+row] = nn
+
+# Save the data to file.
+df_most_common = pd.DataFrame(data=most_common_bins)
+header = ['lower_sep', 'upper_sep'] + ['{}th most common bin'.format(i+1) for i in range(N_most_common)] + ['# in {}th most common bin'.format(i+1) for i in range(N_most_common)]
+df_most_common.to_csv('most_common_bins.csv', header=header, index=False)

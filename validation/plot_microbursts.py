895b89911f697f08353049bcd3997acc5cc7bfee
mike <msshumko@gmail.com>
2019-02-24 16:07:00 -0700

    Updated the microburst_CDF.ipynb file. Nothing major.
    In plot_microbursts.py I started writing the loop method. The loop
    is written and now I need to write the plotting method.

--- validation/plot_microbursts.py
+++ validation/plot_microbursts.py
@@ -3,10 +3,13 @@
 import os
 import numpy as np
 import pandas as pd
+from datetime import datetime, date
 import matplotlib.pyplot as plt
 
 CATALOG_DIR = ('/home/mike/research/ac6_microburst_scale_sizes/data/'
                 'coincident_microbursts_catalogues/')
+AC6_DATA_PATH = lambda sc_id: ('/home/mike/research/ac6/ac6{}/'
+                                'ascii/level2'.format(sc_id))
 
 class PlotMicrobursts:
     def __init__(self, catalog_version, plot_width=5):
@@ -41,7 +44,11 @@ class PlotMicrobursts:
                     ((self.catalog['lon'] > 30)  | (self.catalog['lon'] < -116)) |
                     ((self.catalog['lat'] < -90) | (self.catalog['lat'] > 0))
                                 ]
-
+            for key, vals in filterDict.items():
+                self.catalog = self.catalog[
+                    ((self.catalog[key] > min(vals)) & 
+                    (self.catalog[key] < max(vals)))
+                                ]
         return
 
     def load_catalog(self, catalog_version):
@@ -54,20 +61,43 @@ class PlotMicrobursts:
                 'AC6_coincident_microbursts_v{}.txt'.format(catalog_version))
         self.catalog = pd.read_csv(CATALOG_PATH)
         # Convert the catalog times to datetime objects
-        for timeKey in filter(lambda key: 'time' in key.lower(), self.catalog.keys()):
+        for timeKey in ['dateTime', 'time_spatial_A', 'time_spatial_B']:
             self.catalog[timeKey] = pd.to_datetime(self.catalog[timeKey])
         return
 
     def loop(self):
+        """
+        Loops over the detections that made it through the filters and 
+        make space-time plots.
+        """
+        current_date = date.min
 
+        for _, row in self.catalog.iterrows():
+            if row.dateTime.date() != current_date:
+                # Load current day AC-6 data if not loaded already
+                self.load_ten_hz_data(row.dateTime.date())
+                current_date = row.dateTime.date()
+        
         return
 
-    def load_ten_hz_data(self, date):
+    def load_ten_hz_data(self, day):
         """
         Load the 10 Hz AC6 data from both spacecraft on date.
         """
-
+        time_keys = ['year', 'month', 'day', 'hour', 'minute', 'second']
+        dayStr = '{0:%Y%m%d}'.format(day)
+        pathA = os.path.join(AC6_DATA_PATH('a'), 
+                'AC6-A_{}_L2_10Hz_V03.csv'.format(dayStr))
+        pathB = os.path.join(AC6_DATA_PATH('b'), 
+                'AC6-B_{}_L2_10Hz_V03.csv'.format(dayStr))
+        print(pathA, pathB)
+        self.ac6a_data = pd.read_csv(pathA, na_values='-1e+31')
+        self.ac6a_data['dateTime'] = pd.to_datetime(self.ac6a_data[time_keys])
+        self.ac6b_data = pd.read_csv(pathB, na_values='-1e+31')
+        self.ac6b_data['dateTime'] = pd.to_datetime(self.ac6b_data[time_keys])
         return
 
 if __name__ == '__main__':
     p = PlotMicrobursts(4)
+    p.filter_catalog(filterDict={'Dist_Total':[100, 200]})
+    p.loop()

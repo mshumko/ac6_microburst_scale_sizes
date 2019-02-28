c4dc1d1f082e8c3eee3296d9b0557a9bb776a60a
Mykhaylo Mike Shumko <msshumko@gmail.com>
2019-02-28 11:18:19 -0700

    .gitignore: Added all pyc files.
    microburst_browser_2.py finished the prev/next buttons and it is
    efficient except for when new data needs to be loaded where there
    is a small break.

--- stats/microburst_browser_2.py
+++ stats/microburst_browser_2.py
@@ -22,84 +22,69 @@ class Browser(PlotMicrobursts):
                                 'v{}.txt'.format(catalog_version))
         self.current_date = date.min
         # Subplot objects
-        _, self.ax = plt.subplots(2)
+        _, self.ax = plt.subplots(2, figsize=(8, 7))
+        plt.subplots_adjust(bottom=0.2)
         self.lines = [self.ax[0].plot(), self.ax[1].plot()]
-        self.current_row = self.catalog.iloc[0] # Start at row 0 in the dataframe.
+        self.index = 0 # Start at row 0 in the dataframe.
+        #self.current_row = self.catalog.iloc[self.index] 
         self.plot()
         return
 
-    # def next(self, event):
-    #     # self.ind += 1
-    #     # i = self.ind % len(freqs)
-    #     # ydata = np.sin(2*np.pi*freqs[i]*t)
-    #     l.set_ydata(ydata)
-    #     plt.draw()
+    def next(self, event):
+        """ Plots the next detection """
+        # Just return if at the end of the dataframe.
+        if self.index >= self.catalog.shape[0]:
+            return
+        self.index += 1
+        #self.current_row = self.catalog.iloc[self.index]
+        #self._clear_ax()
+        self.plot()
+        plt.draw()
+        return
 
-    # def prev(self, event):
-    #     self.ind -= 1
-    #     i = self.ind % len(freqs)
-    #     ydata = np.sin(2*np.pi*freqs[i]*t)
-    #     l.set_ydata(ydata)
-    #     plt.draw()
+    def prev(self, event):
+        """ Plots the previous detection """
+        # Just return if at the end of the dataframe.
+        if self.index == 0:
+            return
+        self.index -= 1
+        #self.current_row = self.catalog.iloc[self.index]
+        #self._clear_ax()
+        self.plot()
+        plt.draw()
 
     def plot(self):
         """ 
         Given a self.current_row in the dataframe, make a space-time plot 
         """
-        if self.current_row['dateTime'].date() != self.current_date:
+        current_row = self.catalog.iloc[self.index]
+        if current_row['dateTime'].date() != self.current_date:
             # Load current day AC-6 data if not loaded already
-            self.load_ten_hz_data(self.current_row.dateTime.date())
-        self.make_plot(self.current_row)
+            self.load_ten_hz_data(current_row.dateTime.date())
+            self.current_date = current_row.dateTime.date()
+        self._clear_ax() # Clear anything left over from prior plot.
+        self.make_plot(current_row)
+        self.ax[0].set_title('AC6 Microburst Browser\n {} {}'.format(
+                        current_row['dateTime'].date(), 
+                        current_row['dateTime'].time()))
+        self.ax[0].set_ylabel('mean-subtracted dos1rate\n[counts/s]')
+        self.ax[1].set_ylabel('mean-subtracted dos1rate\n[counts/s]')
+        self.ax[1].set_xlabel('UTC')
+        # self.ax[1].text()
         return
+
+    def _clear_ax(self):
+        [a.clear() for a in self.ax]
+        return 
 
 callback = Browser(6)
 callback.filter_catalog(filterDict={'Dist_Total':[0, 25]})
-# axprev = plt.axes([0.7, 0.05, 0.1, 0.075])
-# axnext = plt.axes([0.81, 0.05, 0.1, 0.075])
-# bnext = Button(axnext, 'Next')
-# bnext.on_clicked(callback.next)
-# bprev = Button(axprev, 'Previous')
-# bprev.on_clicked(callback.prev)
-
-plt.show()
-
-
-########### OLD CODE #############
-# import numpy as np
-# import matplotlib.pyplot as plt
-# from matplotlib.widgets import Button
-
-# freqs = np.arange(2, 20, 3)
-
-# fig, ax = plt.subplots()
-# plt.subplots_adjust(bottom=0.2)
-# t = np.arange(0.0, 1.0, 0.001)
-# s = np.sin(2*np.pi*freqs[0]*t)
-# l, = plt.plot(t, s, lw=2)
-
-# class Index:
-#     ind = 0
-
-#     def next(self, event):
-#         self.ind += 1
-#         i = self.ind % len(freqs)
-#         ydata = np.sin(2*np.pi*freqs[i]*t)
-#         l.set_ydata(ydata)
-#         plt.draw()
-
-#     def prev(self, event):
-#         self.ind -= 1
-#         i = self.ind % len(freqs)
-#         ydata = np.sin(2*np.pi*freqs[i]*t)
-#         l.set_ydata(ydata)
-#         plt.draw()
-
-# callback = Index()
-# axprev = plt.axes([0.7, 0.05, 0.1, 0.075])
-# axnext = plt.axes([0.81, 0.05, 0.1, 0.075])
-# bnext = Button(axnext, 'Next')
-# bnext.on_clicked(callback.next)
-# bprev = Button(axprev, 'Previous')
-# bprev.on_clicked(callback.prev)
-
+axprev = plt.axes([0.59, 0.05, 0.1, 0.075])
+axburst = plt.axes([0.7, 0.05, 0.1, 0.075])
+axnext = plt.axes([0.81, 0.05, 0.1, 0.075])
+bnext = Button(axnext, 'Next')
+bnext.on_clicked(callback.next)
+bprev = Button(axprev, 'Previous')
+bprev.on_clicked(callback.prev)
+bmicroburst = Button(axburst, 'Microburst')
 plt.show()

3723ae545dd4a284d3b5e3bc1d88bbd7c2810d51
mike <msshumko@gmail.com>
2019-04-02 20:42:06 -0600

    Small changes to clean up plots.

--- stats/leo_microburst_cdf.py
+++ stats/leo_microburst_cdf.py
@@ -69,7 +69,7 @@ class Microburst_CDF:
         cdf_std = cdf*np.sqrt((n_prime_std/sum(n_weighted))**2 + 
                             (n_prime_std[0]/sum(n_weighted))**2)
         pdf_std = np.sqrt(cdf_std[1:]**2 + cdf_std[:-1]**2)/self.bin_width
-        return cdf, pdf, cdf_std, pdf_std
+        return cdf, pdf, cdf_std, pdf_std, filtered_catalog.shape[0]
 
     def plot_cdf_pdf(self, L_array=[4, 5, 6, 7, 8], plot_all=True):
         """ Plots the CDF and PDF values. """
@@ -77,41 +77,47 @@ class Microburst_CDF:
         c=['r', 'b', 'g', 'm']
         sample_file_dir = ('/home/mike/research/ac6_microburst'
                             '_scale_sizes/data/norm')
-
+        # Running average parameter. Set to 1 to not average.
+        n = 1
         if plot_all:
             # Plot the CDF over all L shells in the belts.
             self._load_sample_file_(
                 os.path.join(sample_file_dir, 'ac6_norm_all_cdf.csv')
                 )
-            C, P, C_std, P_std = self.calc_cdf_pdf(self.microburst_catalog, 
+            C, P, C_std, P_std, N = self.calc_cdf_pdf(self.microburst_catalog, 
                                                     4, 8)
+            P = np.convolve(np.ones(n)/n, P, mode='same')
             ax[0].errorbar(self.sep_bins[:-1], C, c='k', yerr=C_std, 
-                        label=f'4 < L < 8', lw=3, capsize=5)
-            ax[1].errorbar(self.sep_bins[:-2], P, c='k', yerr=P_std, 
+                        label=f'4 < L < 8 | N = {N}', lw=3, capsize=5)
+            ax[1].errorbar(self.sep_bins[:-2], P, c='k',
                         label=f'4 < L < 8', lw=3)
-            ax[2].plot(self.sep_bins, self.samples.loc[:m.max_sep]/1000, c='k')
+            ax[1].fill_between(self.sep_bins[:-2], P-P_std, P+P_std, facecolor='k',
+                        label=f'4 < L < 8', lw=3, alpha=0.5)
+            ax[2].plot(self.sep_bins, self.samples.loc[:m.max_sep]/10000, c='k')
             
 
         for i, (lower_L, upper_L) in enumerate(zip(L_array[:-1], L_array[1:])):
             self._load_sample_file_(
                 os.path.join(sample_file_dir, f'ac6_norm_{lower_L}_L_{upper_L}_5km_bins.csv')
                 )
-            C, P, C_std, P_std = self.calc_cdf_pdf(self.microburst_catalog, 
+            C, P, C_std, P_std, N = self.calc_cdf_pdf(self.microburst_catalog, 
                                                     lower_L, upper_L)
-            ax[0].errorbar(self.sep_bins[:-1], C, c=c[i], yerr=C_std, 
-                        label=f'{lower_L} < L < {upper_L}', capsize=5)
-            ax[1].errorbar(self.sep_bins[:-2], P, c=c[i], yerr=P_std, 
+            # Running average on the PDF
+            P = np.convolve(np.ones(n)/n, P, mode='same')
+            ax[0].errorbar(self.sep_bins[:-1], C, c=c[i],
+                        label=f'{lower_L} < L < {upper_L} | N = {N}', capsize=5)
+            ax[1].errorbar(self.sep_bins[:-2], P, c=c[i], 
                         label=f'{lower_L} < L < {upper_L}')
             ax[2].plot(self.sep_bins, self.samples.loc[:m.max_sep]/1000, c=c[i])
             
         ax[0].legend()
-        ax[0].set_xlim(left=-1, right=90)
+        ax[0].set_xlim(left=0, right=90)
         ax[0].set_ylim(bottom=0)
         ax[1].set_ylim(bottom=0)
         ax[0].set_ylabel('Microburst fraction')
         ax[1].set_ylabel('Microburst PD')
         ax[2].set_xlabel('Separation [km]')
-        ax[2].set_ylabel('Samples x 1000')
+        ax[2].set_ylabel(r'Samples x $10^4$')
         ax[2].set_ylim(bottom=0)
         ax[2].set_xticks(np.arange(min(self.sep_bins), max(self.sep_bins)+1, 10))
         plt.tight_layout()

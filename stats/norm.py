77647d9b11f9d2c718aaf4d8bded8e2863915275
mike <msshumko@gmail.com>
2019-04-05 13:54:33 -0600

    Working on adding equatorial normalization. I am having IRBEM issues.

--- stats/norm.py
+++ stats/norm.py
@@ -11,8 +11,8 @@ import os
 import pandas as pd
 
 # Import personal libraries
-#sys.path.insert(0, '/home/mike/research/mission-tools/ac6')
 import mission_tools.ac6.read_ac_data as read_ac_data
+import IRBEM
 
 class Hist1D:
     def __init__(self, d=None, startDate=datetime(2014, 1, 1),
@@ -184,6 +184,95 @@ class Hist2D(Hist1D):
             w.writerows(self.count)
         return
 
+class Equatorial_Hist(Hist1D):
+    def __init__(self, sep_bins, hist_key, hist_bins, bins=None, 
+                startDate=datetime(2014, 1, 1),
+                endDate=datetime.now(), modelKwargs={'kext':'OPQ77'},
+                filterDict={}, flag=True):
+        # Initialize init of the parent class.
+        Hist1D.__init__(self, d=None, startDate=startDate, 
+                        endDate=endDate, filterDict=filterDict, flag=flag)
+        # Initialize the equatorial separation bins as well as the histrogram key and 
+        # bins.
+        self.sep_bins = sep_bins
+        self.histKey = hist_key
+        self.hist_bins = hist_bins
+
+        self.count = np.zeros((len(self.sep_bins)-1, len(self.hist_bins)-1))
+
+        # Initialize IRBEM model
+        self.model = IRBEM.MagFields(**modelKwargs)
+        return
+
+    def loop_data(self):
+        """
+        Loops over all of the days between startDate and endDate, loads the 10 Hz
+        data, filters it, maps to equator, and bins the resulting scale sizes.
+        """
+        for day in self.dates:
+            self.load_day_data(day)
+            if (self.ac6dataA is None) or (self.ac6dataB is None):
+                continue # If one (or both) of the data files is empty
+            # ind contains AC6-B indicies that have a correpsonding AC6-A time
+            #  after all of the filtering.
+            ind = self.filterData()
+            # Map to equator
+            d_equator = self.map2equator(ind)
+            # Bin events.
+            # # If using Hist2D, the Hist1D's method will be overwritten.
+            # self.hist_data(ind) 
+
+        return
+
+    def map2equator(self, indB):
+        """ 
+        Maps points from AC6 to the magnetic equator. ind index array
+        is for AC6-B. 
+        """
+        d_equator = np.nan*np.zeros_like(indB)
+
+        # Find corresponding indicies in AC6-A
+        tA = date2num(self.ac6dataA['dateTime'])
+        tB = date2num(self.ac6dataB['dateTime'][indB])
+        indA = np.where(np.in1d(tA, tB, assume_unique=True))[0]
+
+        if len(indA) != len(indB):
+            raise ValueError('indA != indB')
+        # print('here')
+        # indA = np.nan*np.zeros_like(indB)
+        # for i, i_b in enumerate(indB):
+        #     i_a = np.where(self.ac6dataA['dateTime'] == self.ac6dataB['dateTime'][i_b])[0]
+        #     if len(i_a) > 1:
+        #         raise ValueError('More or less than 1 matching AC6-A data point found.')
+        #     indA[i] = i_a[0]
+        # print('there')
+        
+        for i, (i_a, i_b) in enumerate(zip(indA, indB)):
+            # Map to the equator.
+            d_equator[i] = self._equator_mapper(i_a, i_b)
+        return d_equator
+
+    def _equator_mapper(self, i_a, i_b):
+        """ Helper function to format data for IRBEM and map to the equator. """
+        Re = 6371 # Earth radius, km
+        X1 = {'x1':self.ac6dataA['alt'][i_a], 'x2':self.ac6dataA['lat'][i_a], 
+              'x3':self.ac6dataA['lon'][i_a], 
+              'dateTime':self.ac6dataA['dateTime'][i_a].to_pydatetime()}
+        X2 = {'x1':self.ac6dataB['alt'][i_b], 'x2':self.ac6dataB['lat'][i_b], 
+              'x3':self.ac6dataB['lon'][i_b], 
+              'dateTime':self.ac6dataB['dateTime'][i_b].to_pydatetime()}
+        # Run IRBEM
+        X1_equator = self.model.find_magequator(X1, None)['XGEO']
+        X2_equator = self.model.find_magequator(X2, None)['XGEO']
+        print(X1, X2)
+        print(X1_equator, X2_equator, '\n')
+        # Calculate the separations.
+        d_equator = Re*np.linalg.norm(X1_equator-X2_equator)
+        return d_equator
+        # if d_equator > 0:
+        #     return d_equator
+        # else:
+        #     return np.nan
 
 if __name__ == '__main__':
     ### SCRIPT TO MAKE "Dst_Total" NORMALIZATION ###
@@ -225,16 +314,13 @@ if __name__ == '__main__':
     #             bins=[np.arange(2, 10), np.arange(-180, 181, 5)], 
     #             filterDict={'dos1rate':[0, 1E6]})
     # ss.loop_data()
-    sDir = '/home/mike/research/ac6_microburst_scale_sizes/data/norm/'
-    # ss.save_data(os.path.join(sDir, 'ac6_L_lon_bins.csv'), 
-    #              os.path.join(sDir, 'ac6_L_lon_norm.csv'))
-
-    ss2 = Hist2D('Lm_OPQ', 'MLT_OPQ', 
-                    bins=[np.arange(2, 10), np.arange(0, 25)],
-                    filterDict={'dos1rate':[0, 1E6]})
-    ss2.loop_data()
-    ss2.save_data(os.path.join(sDir, 'ac6_L_MLT_bins.csv'), 
-                  os.path.join(sDir, 'ac6_L_MLT_norm.csv'))
+    # sDir = '/home/mike/research/ac6_microburst_scale_sizes/data/norm/'
+    # ss2 = Hist2D('Lm_OPQ', 'MLT_OPQ', 
+    #                 bins=[np.arange(2, 10), np.arange(0, 25)],
+    #                 filterDict={'dos1rate':[0, 1E6]})
+    # ss2.loop_data()
+    # ss2.save_data(os.path.join(sDir, 'ac6_L_MLT_bins.csv'), 
+    #               os.path.join(sDir, 'ac6_L_MLT_norm.csv'))
 
     ### SCRIPT TO MAKE MLT-LON NORMALIZATION ####
 #    ss = Hist2D('MLT_OPQ', 'lon', bins=[np.arange(0, 24.5, 0.5), np.arange(-180, 181, 5)])
@@ -242,3 +328,8 @@ if __name__ == '__main__':
 #    sDir = '/home/mike/research/ac6-microburst-scale-sizes/data/norm/'
 #    ss.save_data(os.path.join(sDir, 'ac6_MLT_lon_bins_2.csv'), 
 #                 os.path.join(sDir, 'ac6_MLT_lon_norm_2.csv'))
+
+    ### SCRIPT TO FIND THE EQUATORIAL NORMALIZATION ###
+    eq = Equatorial_Hist(np.arange(0, 2000, 25), 'Lm_OPQ', np.arange(4, 8.1),
+                        filterDict={'dos1rate':[0, 1E6]})
+    eq.loop_data()

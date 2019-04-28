1ad94dd56960beb2969ef706ae8d5c99764f0c80
Mykhaylo Mike Shumko <msshumko@gmail.com>
2019-04-28 16:52:11 -0600

    Played around with the two-popuation model.

--- models/mcmc_models.py
+++ models/mcmc_models.py
@@ -68,6 +68,52 @@ def mc_brute_force(d, n_bursts=int(1E5), bins=np.arange(0, 100, 5)):
     total_N = np.sum(N)
     cdf = np.array([np.sum(N[i:])/total_N for i in range(len(bins))])
     return cdf
+    
+def mc_brute_vectorized(burst_diamaters, n_bursts=100000, 
+                         bins=np.arange(0, 100, 5), 
+                         grid_size=200):
+    """ 
+    This function tallies events that were see by both spacercaft over 
+    the events seen by one, or both spacercaft as a function of spacecraft 
+    separation.
+    """
+    n = np.zeros(len(bins))
+    
+    # If supplied a single-valued diameter burst_diamaters. If supplied an array, 
+    # I implicityly assume the diameters are distributed according to 
+    # some PDF distribution.
+    if not hasattr(burst_diamaters, '__len__'):
+        burst_diamaters = burst_diamaters*np.ones(n_bursts)
+    # Randomly generate n_burst microburst centers.
+    burst_centers = np.random.uniform(-grid_size, grid_size, size=(n_bursts, 2))
+    
+    ### Calculate which bursts interesect the origin. ###
+    # First calculate the distance each burst is from the origin
+    spacecraft_location = np.zeros_like(burst_centers)
+    distance_to_origin = np.linalg.norm(burst_centers-spacecraft_location, axis=1)
+    # Where close_to_origin is True, the microburst intersected the origin.
+    # We only need to loop over the True values.
+    close_to_origin = np.less(distance_to_origin, burst_diamaters/2)
+    i_close_to_origin = np.where(close_to_origin)[0]
+    # Filter the burst centers to loop over only the ones that contain the origin.
+    burst_centers = burst_centers[i_close_to_origin, :]
+    burst_diamaters = burst_diamaters[i_close_to_origin]
+    
+    # Loop over all spacecraft bins and calculate the subset of the microbursts
+    # that contain the origin also contain the spacecraft at distance d from the
+    # origin along one axis (positive y-axis in this model).
+    for i, d in enumerate(bins):
+        spacecraft_location = np.zeros_like(burst_centers)
+        spacecraft_location[:, 1] = d
+        distance_to_spacecraft = np.linalg.norm(burst_centers-spacecraft_location, 
+                                            axis=1)
+        # Where close_to_origin is True, the microburst intersected the origin.
+        # We only need to loop over the True values.
+        close_to_spacecraft = np.less(distance_to_spacecraft, burst_diamaters/2)
+        n[i] = np.sum(close_to_spacecraft)
+    total_detected = np.sum(n)
+    cdf = np.array([np.sum(n[i:])/total_detected for i in range(len(n))])
+    return cdf
 
 def metroplis(start, target, proposal, niter, nburn=0, 
             thin=1, verbose=False):
@@ -91,16 +137,16 @@ def metroplis(start, target, proposal, niter, nburn=0,
 def Likelihood(p, x, y):
     """ Gaussian likelihood. """
     C = (np.std(y)*np.sqrt(2*np.pi))
-    y_model = (p[0]*mc_brute_force(p[1], bins=x) + 
-              (1-p[0])*mc_brute_force(p[2], bins=x))
+    y_model = (p[0]*mc_brute_vectorized(p[1], bins=x) + 
+              (1-p[0])*mc_brute_vectorized(p[2], bins=x))
     args = sum([(y_i - y_model_i)**2 
                 for (y_i, y_model_i) in zip(y, y_model)])
     return np.exp(-0.5*args/np.var(y))/C
 
 
 if __name__ == '__main__':
-    if os.path.exists(SAVE_PATH):
-        raise ValueError('Data already saved. Use a different savePath. Aborting.')
+#    if os.path.exists(SAVE_PATH):
+#        raise ValueError('Data already saved. Use a different savePath. Aborting.')
 
     # Load the CDF data to model
     cdf_data = pd.read_csv(CDF_DATA_PATH)
@@ -109,8 +155,8 @@ if __name__ == '__main__':
     # Two parameter model. First parameter is the mixing term, and second and third 
     # parameters are the two microburst sizes.
     prior = [scipy.stats.uniform(0, 1), 
-            scipy.stats.uniform(0, 100), 
-            scipy.stats.uniform(0, 100)]
+            scipy.stats.uniform(0, 40), 
+            scipy.stats.uniform(50, 150)]
     # Initial guess on the microburst size.
     start = [prior_i.rvs() for prior_i in prior]
     # How much to jump. Assuming a N(mu, sigma) proposal, ~60% of the time the next proposed jump will be less than proposal_jump km away.
@@ -131,9 +177,9 @@ if __name__ == '__main__':
     target = lambda p: Likelihood(p, cdf_data['Separation [km]'], 
                                 cdf_data['CDF'])*np.prod(
                                 [prior_i.pdf(p_i) for prior_i, p_i in zip(prior, p)])
-    niter = 1000
+    niter = 100000
     trace = metroplis(start, target, proposal, niter, 
-                            nburn=100, thin=1, verbose=False)
+                            nburn=10000, thin=1, verbose=False)
     # Save data
     df = pd.DataFrame(data=trace, columns=['a', 'r0', 'r1'])
     df.to_csv(SAVE_PATH, index=False)

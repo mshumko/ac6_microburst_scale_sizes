4f753211cdb50043b3afcb8a47f47d29254b8640
mike <msshumko@gmail.com>
2019-05-05 20:06:12 -0600

    Sucessfully implemented the generalized analytic model. The double integral is very computationally inneficient, but is elegant and may be easier to explain in the paper.

--- models/analytic_microburst_model.py
+++ models/analytic_microburst_model.py
@@ -1,7 +1,17 @@
 # Now that Paul motivated me to do these integrals.
 import numpy as np
 import scipy.integrate
+import scipy.stats
 import matplotlib.pyplot as plt
+import time
+
+try:
+    import mcmc_maxwell
+    COMPARE_MODEL = True
+except:
+    print('No mcmc_maxwell module found. Will not do a model comparison'
+        ' with the MC model')
+    COMPARE_MODEL = False
 
 def A(r, s):
     """ 
@@ -25,18 +35,55 @@ def one_size_cdf(diameter, s):
     cdf /= np.max(cdf)
     return cdf
 
+def continuous_cdf(s, dist, max_scale=1000):
+    """
+    Calculate the microburst CDF assuming a continuous microburst PDF.
+    s is the AC6 separation array, dist is the scipy.stats distribution
+    evaluated with the distribution parameters, and max_scale is the 
+    upper integral bound for microburst sizes. This bound should technically
+    be np.inf, but to speed it up choose a value where the microburst PDF
+    is really small. 
+    """
+    cdf = np.zeros_like(s, dtype=object)
+    microburst_pdf = lambda x: dist.pdf(x)
+    #r = diameter/2
+    f = lambda r, s_i:A(r, s_i)*microburst_pdf(r) if ~np.isnan(A(r, s_i)) else 0
+
+    for i, s_i in enumerate(s):
+        result = scipy.integrate.dblquad(f, s_i, np.inf, lambda x:0, lambda x:max_scale)
+        cdf[i] = result[0]
+    cdf /= np.max(cdf)
+    return cdf
+
 
 if __name__ == '__main__':
     # New method
     diameter_km = 40 
-    s_array = np.arange(0, 50)
+    s_array = np.linspace(0, 60, num=20)
     cdf = one_size_cdf(diameter_km, s_array)
 
     # Old method
     A_array = A(diameter_km/2, s_array)
-
     cdf_2 = np.array([np.nansum(A_array[i:])/np.nansum(A_array) for i in range(len(s_array))])
 
     plt.plot(s_array, cdf)
     plt.plot(s_array, cdf_2, '--')
     plt.show()
+
+    if COMPARE_MODEL:
+        dist = scipy.stats.maxwell(loc=0, scale=10)
+        # Generalized MC model
+        n_bursts = int(1E6)
+        burst_diameters = dist.rvs(size=n_bursts)
+        cdf_mc = mcmc_maxwell.mc_brute_vectorized(2*burst_diameters, 
+                            bins=s_array, n_bursts=n_bursts)
+        # Fully generalized method
+        cdf_analytic = continuous_cdf(s_array, dist)
+        #print(cdf_3)
+
+        plt.plot(s_array, cdf_mc, label='MC')
+        plt.plot(s_array, cdf_analytic, '--', label='Analytic')
+        plt.legend()
+        plt.show()
+
+    

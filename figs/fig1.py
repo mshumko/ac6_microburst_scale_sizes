d086ffb0b59911852f7c214f001744e0c8490a46
mike <msshumko@gmail.com>
2019-06-11 16:21:50 -0600

    Updated figure 1.

--- figs/fig1.py
+++ figs/fig1.py
@@ -23,7 +23,11 @@ with open('/home/mike/research/ac6_microburst_scale_sizes/data/norm/ac6_L_MLT_no
     norm = 10*np.array(list(reader)).astype(float) # Convert to number of samples.
 
 # Plot the lifetime AC-6 separation
-fig, ax = plt.subplots(2)
+#fig, ax = plt.subplots(2)
+fig = plt.figure(figsize=(11, 5))
+ax = 2*[None]
+ax[0] = plt.subplot(121)
+ax[1] = plt.subplot(122, projection='polar')
 ax[0].plot(r.sepDict['Date/Time'], np.abs(r.sepDict['In-Track Separation [km]']))
 ax[0].set_xlabel('Date [YYYY-MM]')
 ax[0].set_ylabel('Separation [km]')
@@ -36,10 +40,20 @@ ax[0].xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m'))
 # L shell filter for the L-MLT plot
 L_lower = 4
 idL = np.where(np.array(bins['Lm_OPQ']) >= L_lower)[0][0]
-p = ax[1].pcolormesh(bins['MLT_OPQ'], bins['Lm_OPQ'][idL:], norm[idL:, :]/1E5, cmap='Greens')
-plt.colorbar(p, ax=ax[1], label=r'Samples x $10^5$')
+p = ax[1].pcolormesh(np.array(bins['MLT_OPQ'])*np.pi/12, 
+                    bins['Lm_OPQ'][idL:], norm[idL:, :]/1E5, cmap='Reds')
+plt.colorbar(p, ax=ax[1], label=r'10 Hz Samples x $10^5$')
 ax[1].set_xlabel('MLT')
-ax[1].set_ylabel('L')
+ax[1].set_title('AC-6 simultaneous data avaliability')
+#ax[1].set_ylabel('L')
+ax[1].set_theta_zero_location("S") # Midnight at bottom
+mlt_labels = (ax[1].get_xticks()*12/np.pi).astype(int)
+ax[1].set_xticklabels(mlt_labels) # Transform back from 0->2pi to 0->24.
+ax[1].set_yticks([4, 6, 8])
+
+# A and B labels
+ax[0].text(-0.2, 1, '(a)', transform=ax[0].transAxes, fontsize=20)
+ax[1].text(-0.2, 1.05, '(b)', transform=ax[1].transAxes, fontsize=20)
 
 plt.tight_layout()
 plt.show()

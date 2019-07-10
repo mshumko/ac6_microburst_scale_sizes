b97fbb4a6911fe1e8ed7bb4eb314055b8e87a435
mike <msshumko@gmail.com>
2019-07-09 21:26:04 -0600

    Added a pcolormesh plot. May need to refine it a little bit.

--- stats/ac6_AE_vs_s_scatter.py
+++ stats/ac6_AE_vs_s_scatter.py
@@ -4,6 +4,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import dateutil.parser
+import os
 
 import pandas as pd
 
@@ -20,21 +21,37 @@ class AppendIndexToSeparation(AppendGeoMagIdx):
         self.keys = self.dataDict.keys() # For the parent class.
         return
 
-    def saveData(self):
+    def saveData(self, save_path):
         """ Saves data using pandas """
         df = pd.concat(
                 [self.dataDict, pd.DataFrame(self.matchingIndex, columns=['AE'])], 
                 axis=1, names=self.iType)
-        df.to_csv(self.dataPath, index=False)
+        df.to_csv(save_path, index=False)
         return 
 
 if __name__ == '__main__':
     iType = 'ae'
     indexDir = '/home/mike/research/geomag_indicies/ae'
-    separation_file = '/home/mike/research/ac6/AC6_Separation_v2.csv'
+    separation_load_file = '/home/mike/research/ac6/AC6_Separation.csv'
+    separation_save_file = '/home/mike/research/ac6/AC6_Separation_v2.csv'
 
-    appendObj = AppendIndexToSeparation(iType, separation_file, indexDir)
-    appendObj.appendIndex()
-    appendObj.saveData()
+    if not os.path.exists(separation_save_file):
+        appendObj = AppendIndexToSeparation(iType, separation_load_file, indexDir)
+        appendObj.appendIndex()
+        appendObj.saveData(separation_save_file)
+    else:
+        df = pd.read_csv(separation_save_file)#, converters={0:dateutil.parser.parse})
+        # plt.scatter(np.abs(df['In-Track Separation [km]']), df['AE'])
+        # plt.xlabel('Separation [km]'); plt.ylabel('AE'); plt.title('AC6 AE vs Separation')
+        # plt.show()
 
+        # Make a pcolormesh plot of AE vs s.
+        s_bins = np.arange(0, 200, 10)
+        ae_bins = np.arange(0, 1000, 10)
+        H, _, _ = np.histogram2d(np.abs(df['In-Track Separation [km]']), df['AE'], 
+                                bins=(s_bins, ae_bins))
+        im = plt.pcolormesh(s_bins[:-1], ae_bins[:-1], H.T)
+        plt.colorbar(im, label='Number of intervals')
+        plt.xlabel('Separation [km]'); plt.ylabel('AE'); plt.title('AC6 AE vs Separation')
+        plt.show()
 

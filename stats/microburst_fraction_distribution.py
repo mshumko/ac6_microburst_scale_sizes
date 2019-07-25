3fa1a2124669b352481c2d71def2054bb78def5a
mike <msshumko@gmail.com>
2019-07-25 06:54:54 -0600

    Got the fraction of microbursts plotted. I need to check how the error calculation is done, and getting the bin indicies correct.

--- stats/microburst_fraction_distribution.py
+++ stats/microburst_fraction_distribution.py
@@ -42,8 +42,46 @@ class MicroburstFraction:
         """
         return pd.read_csv(path)
 
+    def make_microburst_fraction(self, systematic_error=None,
+                                start_bin=0, end_bin=100, bin_width=5):
+        """ 
+        This method calculates the fraction of coincident to all 
+        microbursts in separation bins defined by start/end_bin and 
+        bin_width kwargs. The systematic_error is the error that is added
+        to the statistical error in quadrature.
+        """
+        self.bins = np.arange(start_bin, end_bin+1, bin_width)
+        self.bin_width = bin_width
+        n = np.nan*np.ones(len(self.bins)-1)
+        n_c = np.nan*np.ones(len(self.bins)-1)
+
+        # Loop over the separation bins and calculate the number of total 
+        # and coincident microburst events.
+        for i, (bin_i, bin_f) in enumerate(zip(self.bins[:-1], self.bins[1:])):
+            n[i] = np.sum((self.microburst_catalog.Dist_Total > bin_i) &
+                          (self.microburst_catalog.Dist_Total < bin_f))
+            n_c[i] = np.sum((self.c_microburst_catalog.Dist_Total > bin_i) &
+                          (self.c_microburst_catalog.Dist_Total < bin_f))
+        self.f = n_c/n
+        if systematic_error is None:
+            self.f_err = self.f*(1-self.f)/np.sqrt(n_c)
+        else:
+            self.f_err = np.sqrt(
+                (self.f*(1-self.f)/np.sqrt(n))**2 + systematic_error**2
+                                )
+        return
+
+    def plot_fraction(self):
+        """ Plot fraction of microbursts with steps and vertical bars for error bars."""
+        plt.step(mf.bins[:-1], mf.f, where='post')
+        plt.errorbar(mf.bins[:-1]+self.bin_width/2, mf.f, ls='', yerr=self.f_err)
+        return
+
 if __name__ == '__main__':
     microburst_name = 'AC6B_microbursts_v5.txt'
     coincident_catalog_name = 'AC6_coincident_microbursts_sorted_Brady_v6.txt'
 
     mf = MicroburstFraction(microburst_name, coincident_catalog_name)
+    mf.make_microburst_fraction()
+    mf.plot_fraction()
+    plt.show()

6e272ae07a5baca5919d392df7e714ef4493a8b5
Mykhaylo Mike Shumko <msshumko@gmail.com>
2019-08-12 13:09:50 -0600

    Paper edits

--- figs/fig1.py
+++ figs/fig1.py
@@ -37,7 +37,7 @@ ax[0].plot(separation['Date/Time'][::sep_downsample],
         np.abs(separation['In-Track Separation [km]'][::sep_downsample]))
 ax[0].set_xlabel('Date [YYYY-MM]')
 ax[0].set_ylabel('Separation [km]')
-ax[0].set_title('AC-6 In-Track Separation')
+ax[0].set_title('AC6 In-Track Separation')
 ax[0].set_yscale('log')
 ax[0].set_ylim(bottom=1)
 ax[0].set_xticks(pd.date_range(start='6/1/2014', end='9/1/2017', freq='3Q'))
@@ -55,18 +55,20 @@ ax[1].fill_between(*earth_shadow, color='k')
 
 # Draw azimuthal lines for a subset of L shells.
 L_labels = [2, 4, 6, 8]
+L_labels_names = [str(i) for i in L_labels[:-1]] + [f'L = {L_labels[-1]}']
 for L in L_labels:
     ax[1].plot(np.linspace(0, 2*np.pi, earth_resolution), 
                 L*np.ones(earth_resolution), ls=':', c='k')
 
 plt.colorbar(p, ax=ax[1], label=r'10 Hz Samples x $10^5$')
 ax[1].set_xlabel('MLT')
-ax[1].set_title('AC-6 simultaneous data avaliability')
+ax[1].set_title('AC6 simultaneous data avaliability')
 #ax[1].set_ylabel('L')
 ax[1].set_theta_zero_location("S") # Midnight at bottom
 mlt_labels = (ax[1].get_xticks()*12/np.pi).astype(int)
 ax[1].set_xticklabels(mlt_labels) # Transform back from 0->2pi to 0->24.
 ax[1].set_yticks(L_labels)
+ax[1].set_yticklabels(L_labels_names)
 
 # A and B labels
 ax[0].text(-0.15, 1.03, '(a)', transform=ax[0].transAxes, fontsize=20)

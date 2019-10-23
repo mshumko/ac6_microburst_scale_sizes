4fea230259d617ab8ec70ad0f46400533c72854e
Mykhaylo Mike Shumko <msshumko@gmail.com>
2019-10-23 11:38:14 -0600

    Updated the cdf error Monte Carlo program. Now I can switch between normal and step plots.
    Also I changed the error percentiles to the 95%
    confidence interval.

--- stats/cdf_error_v2.py
+++ stats/cdf_error_v2.py
@@ -5,6 +5,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 NORM = False
+PLOT_STEP = False
 s_bins_km = np.arange(0, 100, 5)
 
 # Load microburst catalog
@@ -32,37 +33,48 @@ H, _ = np.histogram(cat['Dist_Total'], bins=s_bins_km)
 if NORM:
     H = H * norm
 
-# Now wiggle the number of detections in each bin assuming a 
-# Poisson distribution.
+# Now stocastically adjust the number of detections in each 
+# bin with a Poisson distribution.
 N = 10000
+# shape nS x nTrials
 pdf_trials = np.nan*np.ones((len(H), N), dtype=int)
 for i, H_i in enumerate(H):
+    # In each separation bin, wiggle the number of detections N times.
     pdf_trials[i, :] = np.random.poisson(H_i, size=N)
 
 # Now calculate the CDF for all trials.
-cdf  = 100*np.array([np.sum(H[j:]) for j in range(len(H))])/np.sum(H)
+cdf = 100*np.array([np.sum(H[j:]) for j in range(len(H))])/np.sum(H)
 cdf_trials = np.nan*np.ones_like(pdf_trials)
-for i, pdf_i in enumerate(pdf_trials.T):
-    cdf_trials[:, i] = 100*np.array([np.sum(pdf_i[j:]) for j in range(len(pdf_i))])/np.sum(pdf_i)
+for trial, pdf_i in enumerate(pdf_trials.T):
+    cdf_trials[:, trial] = 100*np.array([np.sum(pdf_i[j:]) for j in range(len(pdf_i))])/np.sum(pdf_i)
 
 # Now calculate the Q1-3 range for the PDF.
-pdf_q = np.percentile(pdf_trials, [25, 75], axis=1)
-cdf_q = np.percentile(cdf_trials, [25, 75], axis=1)
+pdf_q = np.percentile(pdf_trials, [2.5, 97.5], axis=1)
+cdf_q = np.percentile(cdf_trials, [2.5, 97.5], axis=1)
 
 # Plot everything
 fig, ax = plt.subplots(2, sharex=True)
-ax[0].fill_between(s_bins_km[:-1], pdf_q[0, :], pdf_q[1, :], step='post', 
-                    label='Monte Carlo Q1-Q3')
-ax[0].step(s_bins_km[:-1], H, c='r', where='post', label='Cataloged microbursts')
+if PLOT_STEP:
+    ax[1].fill_between(s_bins_km[:-1], pdf_q[0, :], pdf_q[1, :], step='post', 
+                        label='Monte Carlo 95% Interval')
+    ax[1].step(s_bins_km[:-1], H, c='r', where='post', label='Cataloged microbursts')
 
-ax[1].step(s_bins_km[:-1], cdf, c='r', where='post')
-ax[1].fill_between(s_bins_km[:-1], cdf_q[0, :], cdf_q[1, :], step='post', 
-                    label='Monte Carlo Q1-Q3')
+    ax[0].step(s_bins_km[:-1], cdf, c='r', where='post', label='Cataloged microbursts')
+    ax[0].fill_between(s_bins_km[:-1], cdf_q[0, :], cdf_q[1, :], step='post', 
+                        label='Monte Carlo 95% Interval')
+else:
+    ax[1].fill_between(s_bins_km[:-1], pdf_q[0, :], pdf_q[1, :],
+                        label='Monte Carlo 95% Interval')
+    ax[1].plot(s_bins_km[:-1], H, c='r', label='Cataloged microbursts')
+
+    ax[0].plot(s_bins_km[:-1], cdf, c='r', label='Cataloged microbursts')
+    ax[0].fill_between(s_bins_km[:-1], cdf_q[0, :], cdf_q[1, :],
+                        label='Monte Carlo 95% Interval')
 
 ax[0].set_title(f'AC6 Microburst Distribution\nPoisson Error Monte Carlo | Normalized={NORM}')
 ax[1].set_xlabel('AC6 separation [km]')
-ax[0].set_ylabel('Number of microbursts')
+ax[1].set_ylabel('Number of microbursts')
 ax[1].set_xlim(left=0)
-ax[1].set_ylabel('F(s) [%]')
+ax[0].set_ylabel('F(s) [%]')
 ax[0].legend()
 plt.show()

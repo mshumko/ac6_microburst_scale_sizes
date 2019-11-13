21d1a4ccf842a9774f9d3697b3173aca8654f3c4
mike <msshumko@gmail.com>
2019-11-13 15:43:02 -0700

    Working on making the greater than 10 pT distribution also work.

--- rabbit_holes/wave_statistics.py
+++ rabbit_holes/wave_statistics.py
@@ -34,7 +34,7 @@ f_dir = '/home/mike/research/ac6_microburst_scale_sizes/data'
 # JGR2018_F_Fig4_Hchorus_BW_gt_lt_10.sav
 f_name = 'JGR2018_F_Fig4_Hchorus_BW_gt_lt_10.sav' 
 d = scipy.io.readsav(os.path.join(f_dir, f_name))
-dataset_key = 'h_chorus_bwlt10'
+dataset_key = 'h_chorus_bwgt10'
 
 x = np.arange(301)*50 + 25 # 50 km separation bin labels
 y = np.arange(81)/40.-0.9999 # cross-correlation labels from -1 to 1.
@@ -42,6 +42,11 @@ y = np.arange(81)/40.-0.9999 # cross-correlation labels from -1 to 1.
 data_array = d[dataset_key].copy()
 # Estimate the probability density in each separation bin.
 data_array_density = data_array/np.nansum(data_array, axis=0)
+# print(data_array_density, data_array_density.shape, data_array_density[:, 1])
+
+for i in range(20):
+    plt.plot(y, data_array_density[:, i])
+plt.show()
 
 def gaus(x,a,x0,sigma):
     """ 
@@ -52,11 +57,12 @@ def gaus(x,a,x0,sigma):
 # Now fit the profile with Gaussians.
 p = np.nan*np.zeros((x.shape[0], 3)) # Array to store all Gaus parameters
 for i, row in enumerate(data_array_density.T):
+    #print(row, row == 'nan')
+    if np.any(np.isnan(row)):
+        print(i)
+        continue
     p[i, :], _ = scipy.optimize.curve_fit(gaus, y, row, p0=[0.05, 0.7, 0.2])
 
-for i in range(20):
-    plt.plot(y, data_array_density[:, i])
-plt.show()
 
 # Calculate the median and 95% confidence interval of the density
 #detection_stats = np.nanpercentile(data_array_density, [2.5, 50, 97.5], axis=0)
